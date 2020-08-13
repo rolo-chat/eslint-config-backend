4b1ad59bc14b60a9358eaca965abe52c1b2e1a96
Duncan Marsh <duncan@rolo.chat>
2020-08-13 17:39:04 +0800

    [bugfix] removed babel plugin

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   'extends': ['plugin:prettier/recommended', 'eslint:recommended', 'google'],
-  'plugins': ['prettier', 'import', 'babel'],
+  'plugins': ['prettier', 'import'],
   'env': {
     'es6': true,
     'node': true,
