93be811ff92b53452316e10467e00f9486ba7423
Duncan Marsh <duncan@rolo.chat>
2020-08-13 15:42:05 +0800

    [boiler] Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Rolo Backend Eslint Config
---
+++ index.js
@@ -0,0 +1,77 @@
+module.expots = {
+  'extends': ['plugin:prettier/recommended', 'eslint:recommended', 'google'],
+  'plugins': ['prettier', 'import', 'babel'],
+  'env': {
+    'es6': true,
+    'node': true,
+    'mocha': true
+  },
+  'globals': {
+    'expect': true,
+    'chai': true
+  },
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
+    'allowImportExportEverywhere': false,
+    'codeFrame': false
+  },
+  'rules': {
+    'indent': 'off',
+    'eol-last': 'off',
+    'no-invalid-this': 0,
+    'babel/no-invalid-this': 1,
+    'comma-dangle': 'off',
+    'semi': 'off',
+    'key-spacing': 'off',
+    'keyword-spacing': 'off',
+    'quote-props': 'off',
+    'space-before-blocks': 'off',
+    'computed-property-spacing': 'off',
+    'comma-spacing': 'off',
+    'brace-style': 'off',
+    'linebreak-style': ['error', 'unix'],
+    'quotes': 'off',
+    'require-jsdoc': 'off',
+    'max-len': 'off',
+    'operator-linebreak': 'off',
+    'prettier/prettier': ['error'],
+    'new-cap': [
+      'error',
+      {
+        'properties': false
+      }
+    ],
+    'no-throw-literal': 'off',
+    'guard-for-in': 'off',
+    'import/order': [
+      'warn',
+      {
+        'alphabetize': {
+          'order': 'asc',
+          'caseInsensitive': true
+        }
+      }
+    ],
+    'import/no-unresolved': 'error',
+    'no-console': 'error',
+    'no-trailing-spaces': 'off',
+    'space-before-function-paren': 'off'
+  },
+  'settings': {
+    'import/resolver': {
+      'node': {
+        'paths': ['./src']
+      },
+      'babel-module': {
+        'root': './src',
+        'alias': {
+          'test': './test',
+          'env': './env'
+        }
+      }
+    }
+  }
+}
+
