b1761ced1163a42976dd0d39d93df2b08217248c
Duncan Marsh <duncan@rolo.chat>
2020-08-14 17:59:01 +0800

    [feature] engine maps to @kaneoh/rolo-server-engine

--- index.js
+++ index.js
@@ -66,6 +66,7 @@ module.exports = {
       'babel-module': {
         'root': './src',
         'alias': {
+          'engine': '@kaneoh/rolo-server-engine',
           'test': './test',
           'env': './env'
         }
